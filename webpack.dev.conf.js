f1d8973ba179ee940a5e28b06e247411c3d836cf
leonalirong <leonalirong@aliyun.com>
2019-06-18 12:00:52 +0800

    webpack-react

---
+++ webpack.dev.conf.js
@@ -0,0 +1,32 @@
+const path=require('path');
+const HtmWebpackPlugin=require('html-webpack-plugin');
+
+module.exports={
+  entry:'./src/main.js',
+
+  output:{
+    path:path.resolve(__dirname,'./dist'),
+    filename:'[name]-[hash].js'
+  },
+
+  mode:'development',
+
+  module:{
+    rules:[
+      {
+        test:/\.js$/,
+        use:'babel-loader'
+      }
+    ]
+  },
+
+  plugins:[
+    new HtmWebpackPlugin({
+      template:path.resolve(__dirname,'./public/index.html')
+    })
+  ],
+  devServer:{
+    contentBase:'./dist'
+  }
+
+}
